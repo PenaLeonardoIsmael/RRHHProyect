496cc6d5e4db20cbb66db1bc0bb4799bd94d2366
Leonardo Ismael <Leonardo Ismael@DESKTOP-6I5HKGM>
2025-05-21 20:59:22 -0400

    Add project files.

---
+++ RRHHProyect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RRHHProyect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
